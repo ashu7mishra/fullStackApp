2b033b9ff49ac72549e6569eb353138b3194a2b6
ashu7mishra <ashu7mishra@gmail.com>
2025-04-02 08:33:15 +0530

    updating navigation pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,23 @@ import Home from './components/home'
 import Edit from './components/edit'
 import Create from './components/create'
 import Delete from './components/delete'
+import Navbar from './components/navbar/navbar'
 
 function App() {
 
   return (
     <>
-      <Routes>
-          <Route path="" element={<Home/>}/>
-          <Route path="/create" element={<Create/>}/>
-          <Route path="/edit/:id" element={<Edit/>}/>
-          <Route path="/delete/:id" element={<Delete/>}/>
-      </Routes>
+      <Navbar
+        content={
+          <Routes>
+              <Route path="" element={<Home/>}/>
+              <Route path="/create" element={<Create/>}/>
+              <Route path="/edit/:id" element={<Edit/>}/>
+              <Route path="/delete/:id" element={<Delete/>}/>
+          </Routes>
+        }
+      />
+      
     </>
   )
 }
