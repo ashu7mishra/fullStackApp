a43dd8aa30c614b65b4d82f530a57a7424746078
ashu7mishra <ashu7mishra@gmail.com>
2025-04-04 07:30:36 +0530

    connecting backend apis to frontend

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
+import AxiosInstance from "./axios";
 
 const Create = () => {
+    const [country, setCountry] = useState({})
+    const GetData = () => {
+        AxiosInstance.get('country/').then((res) => {
+            setCountry(res.data)
+        })
+    }
     return (
         <div>
             This is the create page
