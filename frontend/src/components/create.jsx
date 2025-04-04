3baf45748d2129a1c45b272854c33d965a961241
ashu7mishra <ashu7mishra@gmail.com>
2025-04-04 07:44:32 +0530

    added country, league and characteristics data to frontend

--- frontend/src/components/create.jsx
+++ frontend/src/components/create.jsx
@@ -1,13 +1,31 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import AxiosInstance from "./axios";
 
 const Create = () => {
-    const [country, setCountry] = useState({})
+    const [country, setCountry] = useState([])
+    const [league, setLeague] = useState([])
+    const [characteristics, setCharacteristics] = useState([])
+
+    console.log('country', country)
+    console.log('league', league)
+    console.log('characteristics', characteristics)
+
     const GetData = () => {
         AxiosInstance.get('country/').then((res) => {
             setCountry(res.data)
         })
+
+        AxiosInstance.get('league/').then((res) => {
+            setLeague(res.data)
+        })
+
+        AxiosInstance.get('characteristics/').then((res) => {
+            setCharacteristics(res.data)
+        })
     }
+    useEffect(() => {
+        GetData()
+    }, [])
     return (
         <div>
             This is the create page
