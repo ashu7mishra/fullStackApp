b6f44002b69f5f76862a4dc6c27cd3e2eb50e0ee
ashu7mishra <ashu7mishra@gmail.com>
2025-04-06 13:28:02 +0530

    hope page complete

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+import { React, useEffect, useMemo, useState } from "react";
+import { Box, Chip, Typography } from "@mui/material";
+import CalendarViewMonthIcon from '@mui/icons-material/CalendarViewMonth';
+import {MaterialReactTable} from 'material-react-table';
+import AxiosInstance from "./axios";
 
 const Home = () => {
+
+    const [myData, setMyData] = useState([])
+
+    const GetData = () => {
+        AxiosInstance.get('footballclub/').then((res) => {
+            setMyData(res.data)
+        })
+
+    }
+    useEffect(() => {
+        GetData()
+    }, [])
+
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: 'name',
+                header: 'Name'
+            },
+            {
+                accessorKey: 'country_details.name',
+                header: 'Country'
+            },
+            {
+                accessorKey: 'league_details.name',
+                header: 'League'
+            },
+            {
+                accessorKey: 'city',
+                header: 'City'
+            },
+            {
+                accessorKey: 'attendance',
+                header: 'Attendance'
+            },
+            {
+                accessorKey: 'characteristics_names',
+                header: 'Characteristics',
+                Cell: ({cell}) => (
+                    <div style={{display:'flex', gap:'8px', flexWrap:'wrap'}}>
+                        {
+                            cell.getValue()?.map((char, index) => (
+                                <Chip key={index} label={char}/>
+                            ))
+                        }
+                    </div>
+                )
+            }
+        ]
+    )
+
     return (
         <div>
-            This is the home page
+            <Box className={"TopBar"}>
+                <CalendarViewMonthIcon/>
+                <Typography sx={{marginLeft:'5px', fontWeight:'bold'}} variant="subtitle2">View all Clubs</Typography>
+            </Box>
+            <MaterialReactTable
+                columns={columns}
+                data={myData}
+            />
         </div>
     )
 }
